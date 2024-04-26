78f332545b3971082d51466fa2f31577cfe5c236
avipatel54 <avipatel8224@gmail.com>
2024-04-26 15:40:50 +0530

    few test cases changes

---
+++ src/test/java/com/cicdTest/CICD/test/UserServiceTest.java
@@ -0,0 +1,41 @@
+package com.cicdTest.CICD.test;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+//@RunWith(MockitoJUnitRunner.class)
+class UserServiceTest {
+
+    @InjectMocks
+    UserService userService;
+
+    @Mock
+    UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void getUserById() {
+        User user=new User("Avi","avipatel8224@gmail.com");
+        Mockito.when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(user));
+        User result = userService.getUserById(12L);
+        assertEquals(user.getName(),"Avi");
+    }
+
+}
